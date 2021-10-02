660fac4e14a9e7c47a9d40f27fba1157ca8f600b
georgeyatsuk@yandex.ru <georgeyatsuk@yandex.ru>
2021-10-02 13:54:23 +0300

    Constructor\distructor in progress

--- IntroductionToOOP/IntroductionToOOP/main.cpp
+++ IntroductionToOOP/IntroductionToOOP/main.cpp
@@ -25,6 +25,26 @@ public:
 	{
 		this->y = y;
 	}
+
+
+	//				 Constructors:
+	Point()
+	{
+		x = y = 0;
+		cout << "DefaultConstructor:\t" << this << endl;
+	}
+	Point(double x = 0, double y = 0)
+	{
+		this->x = x;
+		this->y = y;
+		cout << "Constructor:\t\t" << this << endl;
+	}
+	~Point()
+	{
+		cout << "Destructor:\t\t" << this << endl;
+	}
+
+	//					Methods:
 	double distance(Point other)
 	{
 		double x_distance = this->x - other.x;
@@ -32,11 +52,17 @@ public:
 		double distance = sqrt(x_distance * x_distance + y_distance * y_distance);
 		return distance;
 	}
+	void print()const
+	{
+		cout << "X = " << x << tab << "Y = " << y << endl;
+	}
+
 };
 
 double distance(Point A, Point B);
 
 //#define STRUCT_POINT
+//#define DISTANCE
 
 void main()
 {
@@ -55,6 +81,7 @@ void main()
 	cout << (*pA).x << tab << (*pA).y << endl;
 #endif // STRUCT_POINT
 
+#ifdef DISTANCE
 	Point A;
 	A.set_x(2);
 	A.set_y(3);
@@ -69,6 +96,21 @@ void main()
 	cout << endl;
 	cout << distance(A, B) << endl;
 	cout << distance(B, A) << endl;
+
+#endif // DISTANCE
+
+	//for (int i = 0; i < 10; i++)
+	//{
+	//	cout << i << tab;
+	//}
+	//cout << endl;
+	Point A;		//Default constractor
+	A.print();
+	Point B(2, 3);
+	B.print();
+
+	//Point(); -	Конструктор класса Point
+	//~Point(); -	Деструктор класса Point
 }
 
 double distance(Point A, Point B)
