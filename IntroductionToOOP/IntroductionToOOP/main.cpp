79ce2fb65966ebcfff4a4bc5023807c3c65c8f99
georgeyatsuk@yandex.ru <georgeyatsuk@yandex.ru>
2021-10-02 12:24:26 +0300

    Distance between Points

--- IntroductionToOOP/IntroductionToOOP/main.cpp
+++ IntroductionToOOP/IntroductionToOOP/main.cpp
@@ -9,9 +9,34 @@ class Point
 	double x;
 	double y;
 public:
+	double get_x()const
+	{
+		return x;
+	}
+	double get_y()const
+	{
+		return y;
+	}
+	void set_x(double x)
+	{
+		this->x = x;
+	}
+	void set_y(double y)
+	{
+		this->y = y;
+	}
+	double distance(Point other)
+	{
+		double x_distance = this->x - other.x;
+		double y_distance = this->y - other.y;
+		double distance = sqrt(x_distance * x_distance + y_distance * y_distance);
+		return distance;
+	}
 };
 
-#define STRUCT_POINT
+double distance(Point A, Point B);
+
+//#define STRUCT_POINT
 
 void main()
 {
@@ -30,4 +55,29 @@ void main()
 	cout << (*pA).x << tab << (*pA).y << endl;
 #endif // STRUCT_POINT
 
+	Point A;
+	A.set_x(2);
+	A.set_y(3);
+	cout << A.get_x() << tab << A.get_y() << endl;
+
+	Point B;
+	B.set_x(4);
+	B.set_y(5);
+	//distance - возвращает расстояние до указанной точки
+	cout << A.distance(B) << endl;
+	cout << B.distance(A) << endl;
+	cout << endl;
+	cout << distance(A, B) << endl;
+	cout << distance(B, A) << endl;
+}
+
+double distance(Point A, Point B)
+{
+	//double x_distance = A.get_x() - B.get_x();
+	//double y_distance = A.get_y() - B.get_y();
+	//double distance = sqrt(x_distance * x_distance + y_distance * y_distance);
+	//return distance;
+	//return sqrt((A.get_x() - B.get_x()) * (A.get_x() - B.get_x()) + (A.get_y() - B.get_y()) * (A.get_y() - B.get_y()));
+	return sqrt(pow(A.get_x() - B.get_x(), 2) + (pow(A.get_y() - B.get_y(), 2)));
+	//return pow(pow(A.get_x() - B.get_x(), 2) + (pow(A.get_y() - B.get_y(), 2)), 0.5);
 }
